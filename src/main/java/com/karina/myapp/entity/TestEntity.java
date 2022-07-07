348ad3e351becbe9c110f7426a3d6cb377eb0607
Karina.Sanchez <karoty_25@hotmail.com>
2022-07-07 10:13:42 -0500

    Nombre del Proyecto: my-app, Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/karina/myapp/entity/TestEntity.java
@@ -0,0 +1,29 @@
+package com.karina.myapp.entity;
+
+/**
+ * @author ksanchezpu
+ */
+
+public class TestEntity {
+
+	/**
+	 * Propiedad de prueba para la muestra de compilación del archivo testentity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
